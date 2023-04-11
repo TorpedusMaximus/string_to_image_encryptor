c58e73c5288d9c24c56befda58f6cb72fe1ee11a
Kacper Małkowski <kacper.malkowski@estigiti.com>
2023-04-11 21:48:26 +0200

    Decryption part

---
+++ src/decryptor.rs
@@ -0,0 +1,34 @@
+pub mod decryptor {
+    use image::open;
+
+    pub fn decrypt() {
+        let mut path = String::new();
+        println!("Path to image :");
+        std::io::stdin().read_line(&mut path).unwrap();
+        path.remove(path.len()-1);
+        if path=="" {
+            path.push_str("image.png");
+        }
+
+
+        let mut image_values = open(path).unwrap().into_luma8().to_vec();
+        image_values.reverse();
+
+        for i in 0..image_values.len() {
+            if image_values[i] != 0 {
+                image_values.splice(0..(i), Vec::new()); // possibly i-1 ??????
+                break;
+            }
+        }
+        image_values.reverse();
+
+        let mut line = String::new();
+
+        for i in 0..image_values.len() {
+            let character = image_values[i] as char;
+            line.push(character);
+        }
+
+        println!("{:?}", line)
+    }
+}
