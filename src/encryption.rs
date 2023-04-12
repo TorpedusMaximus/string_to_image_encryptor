ef201447dbb3e51f04f93ac998f62cb86d281888
Kacper Małkowski <kacper.malkowski@estigiti.com>
2023-04-12 11:58:21 +0200

    Add smaller size

--- src/encryption.rs
+++ src/encryption.rs
@@ -9,7 +9,6 @@ pub mod encryptor {
         std::io::stdin().read_line(&mut line).unwrap();
 
         let line_length = line.len() as u32;
-
         let mut counter = 1u32;
 
         loop {
@@ -21,12 +20,12 @@ pub mod encryptor {
         }
 
         let n = counter;
-        let image_size = (n * n) as usize;
+        let exact_h = f64::from(line_length) / f64::from(n);
+        let h = exact_h.ceil() as u32;
+        let image_size = (n * h) as usize;
 
-        let mut img = GrayImage::new(n, n);
-
+        let mut img = GrayImage::new(n, h);
         let mut pixels_values = vec![Luma([0u8]); image_size];
-
 
         for (index, character) in line.chars().enumerate() {
             let character_in_ascii = character as u8;
@@ -35,7 +34,7 @@ pub mod encryptor {
 
         let mut pixel_id = 0;
 
-        for x in 0..n {
+        for x in 0..h {
             for y in 0..n {
                 let pixel = pixels_values[pixel_id];
                 img.put_pixel(y, x, pixel);
@@ -46,7 +45,7 @@ pub mod encryptor {
         let mut path = String::new();
         println!("Path to image :");
         std::io::stdin().read_line(&mut path).unwrap();
-        path.remove(path.len()-1);
+        path.remove(path.len() - 1);
         if path.is_empty() {
             path.push_str("image.png");
         }
